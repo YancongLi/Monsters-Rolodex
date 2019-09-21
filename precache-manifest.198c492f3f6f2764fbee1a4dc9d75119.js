9bd44f65ebbfb4f22a85f7c871d8e4fd0b3ac3c3
Peter <yancong.li@alumni.ubc.ca>
2019-09-21 14:16:56 -0700

    Updates

---
+++ precache-manifest.198c492f3f6f2764fbee1a4dc9d75119.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18aa83fedbb98c4efbf910afde75476c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "08eda94a4c5ca42bfd23",
+    "url": "/monsters-rolodex/static/css/main.e18d0459.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "08eda94a4c5ca42bfd23",
+    "url": "/monsters-rolodex/static/js/main.ae068ed4.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
